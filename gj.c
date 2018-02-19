327fc529f4712dd10e80cbf09b062c768678a7e4
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-19 13:19:47 +0530

    gj.c

---
+++ gj.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[100], b[100];    
+    printf("Enter the first string\n");    
+    gets(a);    
+
+      
+    gets(b);
+    
+    printf("Entered strings are equal.\n");
+        printf("Entered strings are not equal.\n");
+        return 0;
+}
