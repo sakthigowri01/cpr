614c88cdfa3016bbb368e1a4dd9174c7ae7ab5ac
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-19 13:43:15 +0530

    gowrisathi.c

---
+++ gowrisathi.c
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+
+    
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+   for(int i=1;i<=a;i++){
+       f=f+i;
+   }
+     int g=f/a;
+     System.out.println(g);
+     
